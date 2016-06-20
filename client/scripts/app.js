71f8dc32ffd9c8c5d6aba9e0a0ad99681e2c886d
kwong3672 <keith.wong3672@gmail.com>
2016-06-20 15:41:43 -0700

    Installed bower, installed underscore, added our key and id, created object app, created our methods init and send, created working AJAX get and post functions.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,43 @@
 // YOUR CODE HERE:
+var app = {
+  username: 'hien',
+  text: 'hey',
+  roomname: 'hey'
+};
 
+app.init = function () {
+  return true;
+};
+
+app.send = function () {
+  return true;
+};
+
+var data;
+
+$(document).ready(function() {
+  // $.ajax({
+  //   url: 'https://api.parse.com/1/classes/messages',
+  //   data: JSON.stringify(app),
+  //   type: 'POST',
+  //   success: function() {
+  //     console.log('post success');
+  //   },
+  //   error: function() {
+  //     console.log('post rejected');
+  //   }
+  // });
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    dataType: 'json',
+    data: data,
+    // contentType: 'application/json', 
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+});
