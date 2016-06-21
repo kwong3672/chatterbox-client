49d9ee033568e3ee59a150554d6d36cbc750c606
kwong3672 <keith.wong3672@gmail.com>
2016-06-20 17:24:50 -0700

    Added div with id roomSelect inside chat div in index.html Complete fetch method, completed clear messages from DOM method, completed add messages to DOM method, completed a addRooms method
    Fixed init to seperate messages by roomName

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -15,8 +15,14 @@ app.init = function () {
     // contentType: 'application/json', 
     success: function(data) {
       _.each(data.results, function(message) {
+        message.roomname = message.roomname || 'none';
+        var room = message.roomname.split(' ').join(''); 
+        if ( !$('#' + room ).length ) {
+          var $room = '<div id=' + room + '></div>';
+          $('#roomSelect').append($room);
+        }
         var $msg = '<div>' + message.username + ':' + message.text + '</div>';
-        $('#chats').append($msg);
+        $('#' + room).append($msg);
       });
     },
     error: function(data) {
@@ -38,6 +44,45 @@ app.send = function (message) {
       console.log('post rejected');
     }
   });
+};
+
+app.fetch = function () {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    dataType: 'json',
+    data: data,
+    // contentType: 'application/json', 
+    success: function(data) {
+      _.each(data.results, function(message) {
+        var $msg = '<div class="chatdiv">' + message.username + ':' + message.text + '</div>';
+        $('#chats').append($msg);
+      });
+      return app.server;
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+};
+
+app.server = 'https://api.parse.com/1/classes/messages';
+
+app.clearMessages = function () {
+  $('#chats').html('');
+};
+
+app.addMessage = function (msg) {
+  app.send(msg);
+  var $msg = '<div>' + msg.username + ':' + msg.text + '</div>';
+  $('#chats').append($msg);
+};
+
+app.addRoom = function (room) {
+  if ( !$('#' + room ).length ) {
+    var $room = '<div id=' + room + '></div>';
+    $('#roomSelect').append($room);
+  }
 };
 
 var data;
